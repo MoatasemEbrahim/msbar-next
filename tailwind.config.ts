2303ac43af32a7f1b755e9f96bdef71acf27a4c1
moatasemEbrahim <moatasem@carry1st.com>
2024-06-30 03:04:29 +0300

    test app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,6 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -18,3 +16,4 @@ const config: Config = {
   plugins: [],
 };
 export default config;
+
